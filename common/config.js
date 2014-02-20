cf0cadd2ed6b1b0a6932dec5aa13bfdcc0e441ba
Stephen Coward <stephendcoward@gmail.com>
2014-02-20 15:10:11 -0600

    Starting over

--- common/config.js
+++
@@ -1,5 +0,0 @@
-Meteor.metamech = Meteor.metamech || {}
-
-Meteor.metamech.Messages = new Meteor.Collection('messages')
-Meteor.metamech.Rooms = new Meteor.Collection('rooms')
-Meteor.metamech.Counts = new Meteor.Collection('counts')
--- dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-NODE_OPTIONS='--debug' MONGO_URL="mongodb://127.0.0.1:27017/groupchat_dev" meteor
