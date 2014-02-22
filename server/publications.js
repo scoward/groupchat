9559c364e73026d4b043c765183d4451e6cf4630
Stephen Coward <stephendcoward@gmail.com>
2014-02-22 02:33:51 -0600

    Getting message counts and show more button working

--- server/publications.js
+++ server/publications.js
@@ -1,57 +1,24 @@
 // Chatrooms
 Meteor.publish('chat', function(params) {
+    // TODO ROOMS SHOULD NOT BE CREATED HERE, CHANGE THIS...SOMEHOW
     if (params.type == 'github') {
         var room = Meteor.metamech.Rooms.findOne({owner: params.owner, repo: params.repo, type: params.type})
             , roomId
+            , gt
     
         if (room == null) {
             roomId = Meteor.metamech.Rooms.insert({owner: params.owner, repo: params.repo, type: params.type})
+            gt = -1
         } else {
             roomId = room._id
+            gt = room.count - params.offset
         }
 
-        // Need to limit the publish or too much data will be sent over the wire.
-        if (params.limit == null) {
-            params.limit = 50
-        }
-
-        return Meteor.metamech.Messages.find({room: roomId}, {sort: {timestamp: -1}, limit: params.limit})
+        return Meteor.metamech.Messages.find({room: roomId, count: {$gt: gt}}, {sort: {timestamp: -1}})
     }
     return []
 })
 
 Meteor.publish('room', function(params) {
     return Meteor.metamech.Rooms.find({owner: params.owner, repo: params.repo, type: params.type})
-})
-
-// Publish the amount of messages in a chatroom to the "counts" collection
-Meteor.publish('counts', function(params) {
-    var self = this
-        , count = 0
-        , initializing = true
-    var room = Meteor.metamech.Rooms.findOne({owner: params.owner, repo: params.repo, type: params.type})
-        , roomId = room._id
-    var handle = Meteor.metamech.Messages.find({room: roomId}).observeChanges({
-        added: function(id) {
-            count++
-            if (!initializing) {
-                self.changed("counts", roomId, {count: count})
-            }
-        },
-        removed: function(id) {
-            count--;
-            self.changed("counts", roomId, {count: count})
-        }
-    })
-
-    // Only send changes after the initial added calls are made.
-    // Mark subscription as ready and return initial count.
-    intializing = false
-    self.added("counts", roomId, {count: count})
-    self.ready()
-
-    // Stop observing the cursor when a client unsubs.
-    self.onStop(function() {
-        handle.stop()
-    })
 })
